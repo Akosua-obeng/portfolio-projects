38df903300bdf753cf54d52646cb48d949c84b93
Akosua <ekyieakosua@gmail.com>
2023-09-04 12:10:15 +0000

    update of capstone after adding logos

---
+++ src/Components/Footer.js
@@ -0,0 +1,11 @@
+import footerlogo from "../assets/footerlogo.png";
+
+function Footer(){
+  return(
+    <footer>
+      <img src={footerlogo} alt="footerlogo"/>
+    </footer>
+  )
+}
+
+export default Footer;
